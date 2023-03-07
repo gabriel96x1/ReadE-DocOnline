2c3e021ae25b5b6683937950b1f48a98fcaf666c
gabriel96x1 <gabriel96x1@gmail.com>
2023-03-07 13:45:58 -0600

    fix: fix text align

--- app/src/main/java/com/mutablestate/readonline/presentation/view/AnalyzingInfoScreen.kt
+++ app/src/main/java/com/mutablestate/readonline/presentation/view/AnalyzingInfoScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,7 +29,8 @@ fun AnalyzingInfoScreen() {
             Text(
                 text = stringResource(R.string.reading_id_message_analyzing_info_screen),
                 fontSize = 20.sp,
-                modifier = Modifier.weight(.3f)
+                modifier = Modifier.weight(.3f),
+                textAlign = TextAlign.Center
             )
             LottieAnimation(
                 composition = composition,
