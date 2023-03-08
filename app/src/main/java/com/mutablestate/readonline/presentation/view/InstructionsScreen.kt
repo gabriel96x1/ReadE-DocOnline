f8fa82cb0d337a46333d2eb3b9c1c9382103bd0d
gabriel96x1 <gabriel96x1@gmail.com>
2023-03-08 13:40:20 -0600

    feat: more aid on instruccions screen

--- app/src/main/java/com/mutablestate/readonline/presentation/view/InstructionsScreen.kt
+++ app/src/main/java/com/mutablestate/readonline/presentation/view/InstructionsScreen.kt
@@ -1,13 +1,23 @@
 package com.mutablestate.readonline.presentation.view
 
 import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,8 +29,12 @@ import com.mutablestate.readonline.presentation.activities.CameraActivity
 @Composable
 fun InstructionsScreen() {
     val context = LocalContext.current
+    val instructionsVisibility = remember {
+        mutableStateOf(false)
+    }
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(20.dp),
         horizontalAlignment = Alignment.Start,
@@ -51,11 +65,42 @@ fun InstructionsScreen() {
             textAlign = TextAlign.Justify
         )
         Spacer(modifier = Modifier.height(12.dp))
-        Text(
-            text = stringResource(R.string.recom2_inst_screen),
-            fontSize = 14.sp,
-            textAlign = TextAlign.Justify
-        )
+        Row {
+            Text(
+                text = stringResource(R.string.recom2_inst_screen),
+                fontSize = 14.sp,
+                textAlign = TextAlign.Justify
+            )
+            Icon(
+                painter = painterResource(id = if (instructionsVisibility.value) R.drawable.arrow_drop_down else R.drawable.arrow_drop_up), 
+                contentDescription = stringResource(R.string.more_inst_inst_screen),
+                modifier = Modifier.clickable { instructionsVisibility.value = !instructionsVisibility.value }
+            )
+        }
+        
+        if (instructionsVisibility.value) {
+            Spacer(modifier = Modifier.height(12.dp))
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.pantalla_cut),
+                    contentDescription = stringResource(R.string.example_image_inst_screen),
+                    modifier = Modifier.rotate(270f),
+                    contentScale = ContentScale.Fit
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.passport_image_crop),
+                    contentDescription = stringResource(R.string.id_image_inst_screen),
+                    modifier = Modifier
+                        .fillMaxSize(.6f)
+                        .padding(end = 40.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
+        }
+        
         Spacer(modifier = Modifier.height(12.dp))
         Text(
             text = stringResource(R.string.recom3_inst_screen),
